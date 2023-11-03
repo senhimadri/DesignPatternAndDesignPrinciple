04884db8f094abf80ce91f00924cc9ca90a56d96
senhimadri <senhimadri.dev@gmail.com>
2023-11-03 08:48:08 +0600

    make pre code

--- LSP_ClassLibrary_IamTimCorey/OrginalCode/Manager.cs
+++ LSP_ClassLibrary_IamTimCorey/OrginalCode/Manager.cs
@@ -10,6 +10,11 @@ public class Manager : Employee
 {
     public override void CalculatePerHourRate(int rank)
     {
+        if (rank< 0  || rank >5)
+        {
+            throw new Exception();
+        }
+
         decimal baseAmount = 19.75M;
         Salary = baseAmount + (rank * 4);
     }
