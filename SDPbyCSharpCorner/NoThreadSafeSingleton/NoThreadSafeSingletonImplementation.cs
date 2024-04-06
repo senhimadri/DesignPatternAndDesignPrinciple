844b1220f43552d5ac10316360be50d2eaff8adf
senhimadri <senhimadri.dev@gmail.com>
2024-04-06 09:11:06 +0600

    Why NoThreadSafeSingleton is not safe.

--- SDPbyCSharpCorner/NoThreadSafeSingleton/NoThreadSafeSingletonImplementation.cs
+++ SDPbyCSharpCorner/NoThreadSafeSingleton/NoThreadSafeSingletonImplementation.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
+using System.Runtime.Intrinsics.Arm;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SDPbyCSharpCorner.NoThreadSafeSingleton;
 internal class NoThreadSafeSingletonImplementation
 {
-    NoThreadSafeSingletonClass singleton = new NoThreadSafeSingletonClass();
+    NoThreadSafeSingletonClass singleton = NoThreadSafeSingletonClass.Instance;
+    //Why it is not safe?
+
+    // In a multi-threaded environment, if two or more threads simultaneously access the "Instance" property when "instance" is null,
+    // they may end up creating multiple instances of the class. This is a violation of the SDP principle.
+
+
 
 
 }
