315c98755365cb1e6d474858d20c00677821c0a0
senhimadri <senhimadri.dev@gmail.com>
2024-04-06 08:36:37 +0600

    No Thread Safe Singleton Design Pattern is Implemented

---
+++ SDPbyCSharpCorner/NoThreadSafeSingleton/NoThreadSafeSingleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDPbyCSharpCorner.NoThreadSafeSingleton;
+
+public sealed class NoThreadSafeSingleton
+{
+    private NoThreadSafeSingleton() { }
+    private static NoThreadSafeSingleton? instance = null;
+
+
+    private static NoThreadSafeSingleton Instance { 
+    
+        get
+        {
+            if (instance is null)
+            {
+                instance = new NoThreadSafeSingleton();
+            }
+            return instance;
+        }
+    }
+}
+
