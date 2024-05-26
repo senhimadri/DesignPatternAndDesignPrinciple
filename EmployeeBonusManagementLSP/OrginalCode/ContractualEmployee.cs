116e028cbd7272c27c8a08c13167e7e7baa08231
senhimadri <senhimadri.dev@gmail.com>
2024-05-26 08:31:42 +0600

    Just a Namespace change in LSP

---
+++ EmployeeBonusManagementLSP/OrginalCode/ContractualEmployee.cs
@@ -0,0 +1,14 @@
+﻿namespace EmployeeBonusManagementLSP.OrginalCode;
+
+public class ContractualEmployee : Employee
+{
+    public ContractualEmployee(int _EmployeeId, string _EmployeeName, decimal _Salary) : base(_EmployeeId, _EmployeeName, _Salary)
+    {
+
+    }
+    public override decimal CalculateBonus()
+    {
+        throw new Exception("Contractual Employee don't have the bonus.");
+    }
+}
+
--- EmployeeBonusManagementLSP/OrginalCode/PorationaryEmployee.cs
+++ EmployeeBonusManagementLSP/OrginalCode/PorationaryEmployee.cs
@@ -12,15 +12,3 @@ public class PorationaryEmployee : Employee
     }
 }
 
-public class ContractualEmployee : Employee
-{
-    public ContractualEmployee(int _EmployeeId, string _EmployeeName, decimal _Salary) : base(_EmployeeId, _EmployeeName, _Salary)
-    {
-
-    }
-    public override decimal CalculateBonus()
-    {
-        throw new Exception("Contractual Employee don't have the bonus.");
-    }
-}
-
