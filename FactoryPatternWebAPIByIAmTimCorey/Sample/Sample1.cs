4d98947973b6021239d9396b656cf5a01dfaa739
senhimadri <senhimadri.dev@gmail.com>
2024-06-04 07:50:43 +0600

    PProject Created For Factory Design Pattern

---
+++ FactoryPatternWebAPIByIAmTimCorey/Sample/Sample1.cs
@@ -0,0 +1,13 @@
+﻿namespace FactoryPatternWebAPIByIAmTimCorey.Sample;
+
+public class Sample1: ISample1
+{
+    public string CurrentDateTime { get; set; } = DateTime.Now.ToString();
+}
+
+public interface ISample1
+{
+    string CurrentDateTime { get; set; }
+}
+
+
---
+++ FactoryPatternbyIAmTimCorey/Sample/Sample1.cs
@@ -0,0 +1,11 @@
+﻿namespace FactoryPatternbyIAmTimCorey.Sample;
+
+public class Sample1 : ISample1
+{
+    public string CurrentDateTime { get; set; } = DateTime.Now.ToString("yyyy-MM-dd");
+}
+
+public interface ISample1
+{
+    string CurrentDateTime { get; set; }
+}
