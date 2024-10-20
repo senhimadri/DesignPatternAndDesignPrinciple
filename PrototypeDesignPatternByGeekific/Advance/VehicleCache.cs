4beb3c3c85e4e17366695cf0c12af60e03b68859
senhimadri <senhimadri.dev@gmail.com>
2024-10-20 08:14:16 +0600

    checked

--- PrototypeDesignPatternByGeekific/Advance/VehicleCache.cs
+++ PrototypeDesignPatternByGeekific/Advance/VehicleCache.cs
@@ -6,8 +6,14 @@ internal class VehicleCache
 
 	public VehicleCache()
 	{
-		Car car = new Car(brand: "Bugatti",model: "M21", color: "Black", topSpeed: 300);
-        Bus bus = new Bus(brand: "Volvo", model: "XL34567", color: "White", doors: 3);
+		Car car = new Car(brand: "Bugatti",
+                          model: "M21", 
+                          color: "Black", 
+                          topSpeed: 300);
+        Bus bus = new Bus(brand: "Volvo", 
+                          model: "XL34567", 
+                          color: "White", 
+                          doors: 3);
 
         cache.Add("Bugatti",car);
         cache.Add("Volvo", bus);
