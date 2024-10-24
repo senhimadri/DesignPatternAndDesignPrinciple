b70b75a709409386e783e9bb50f4d71378c22851
senhimadri <senhimadri.dev@gmail.com>
2024-10-24 17:34:32 +0600

    Singleton Design Pattern Implemented

---
+++ SingletonDesignPatternByGFG/Singleton.cs
@@ -0,0 +1,23 @@
+﻿namespace SingletonDesignPatternByGFG.PrivateConstructor;
+
+public class Singleton
+{
+	private static Singleton? instance;
+	private Singleton()
+	{
+		Console.WriteLine("Singleton is Instantiated."); ;
+	}
+
+	public static Singleton GetInstance()
+	{
+		if (instance is null)
+			instance = new Singleton();
+
+		return instance;
+	}
+
+	public void DoSomething()
+	{
+		Console.WriteLine("Something is Done.");
+	}
+}
