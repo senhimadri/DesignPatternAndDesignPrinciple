1f407ac52f7a43915c26deb39192c10f3b36ce6e
senhimadri <senhimadri.dev@gmail.com>
2024-10-27 21:40:12 +0600

    Adopter Pattern Implemented

---
+++ AdopterDesignPatternByGFG/IPrinter.cs
@@ -0,0 +1,38 @@
+﻿namespace AdopterDesignPatternByGFG;
+
+public interface IPrinter
+{
+    void PrintDocument(string document);
+}
+
+public class LegacyPrinter
+{
+    public void OldPrint(string text)
+    {
+        Console.WriteLine($"Printing from LegacyPrinter: {text}");
+    }
+}
+
+
+public class PrinterAdopter : LegacyPrinter, IPrinter
+{
+    public void PrintDocument(string document)
+    {
+        OldPrint(document);
+    }
+}
+
+public class Client
+{
+    private readonly IPrinter _printer;
+
+    public Client(IPrinter printer)
+    {
+        _printer = printer;
+    }
+
+    public void Print(string document)
+    {
+        _printer.PrintDocument(document);
+    }
+}
