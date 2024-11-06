442316eee58a5d4aaca0bbc65a7d33fd80623a48
senhimadri <senhimadri.dev@gmail.com>
2024-11-06 19:35:12 +0600

    Proxy Design Pattern Implementation

---
+++ ProxyDesignPatternbyGfG/ImageInterface.cs
@@ -0,0 +1,59 @@
+﻿namespace ProxyDesignPatternbyGfG;
+
+public interface ImageInterface
+{
+    void Display();
+}
+
+public class RealImage : ImageInterface
+{
+    private string filename;
+    public RealImage(string filename)
+                                => this.filename = filename;
+
+    public void LoadImageFromDisk()
+    {
+        Console.WriteLine($"Loading image {filename}");
+    }
+    
+    public void Display()
+    {
+        Console.WriteLine($"Displaying image {filename}");
+    }
+}
+
+public class ProxyImage : ImageInterface
+{
+    private RealImage? realImage;
+    private string fileName;
+
+    public ProxyImage(string fileName)
+    {
+        this.fileName = fileName;
+    }
+
+    public void Display()
+    {
+        if (realImage is null)
+        {
+            realImage = new RealImage(fileName);
+        }
+
+        realImage.Display();
+    }
+}
+
+public class ProxyPatternImplementation
+{
+    public static void Main()
+    {
+        ImageInterface image = new ProxyImage("example.jpg");
+
+        image.Display();
+
+        image.Display();
+
+    }
+}
+
+
