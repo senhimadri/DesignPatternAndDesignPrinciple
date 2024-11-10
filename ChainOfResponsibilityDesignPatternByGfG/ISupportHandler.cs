90d1a644b65959764013d0e4a66e5b168c5a9755
senhimadri <senhimadri.dev@gmail.com>
2024-11-10 07:24:57 +0600

    feat: Handler Interface Created

---
+++ ChainOfResponsibilityDesignPatternByGfG/ISupportHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace ChainOfResponsibilityDesignPatternByGfG;
+
+public interface ISupportHandler
+{
+    void HandleRequest();
+    void SetNextHandler();
+
+}
+
+
+public class Request(Priority _priority)
+{
+    private Priority priority = _priority;
+    public Priority GetPriority() => priority;
+}
+
+
+public enum Priority
+{
+    Basic, Intermediate, Critical
+}
+
