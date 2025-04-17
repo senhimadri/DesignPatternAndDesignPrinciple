745f0dd3ad648768eb35301d01874e2809fcd562
senhimadri <senhimadri.dev@gmail.com>
2025-04-17 07:28:35 +0600

    Doc(BuilderDesignPattern): Computer Assembly implemented using builder design pattern

---
+++ BuilderDesignPatternExercise/ComputerAssembly.cs
@@ -0,0 +1,66 @@
+﻿namespace BuilderDesignPatternExercise;
+
+//1.Product
+public class Computer
+{
+    private string? cpu;
+    private string? ram;
+    private string? storage;
+
+    public void SetCpu(string cpu) => this.cpu = cpu;
+    public void SetRam(string ram) => this.ram = ram;
+    public void SetStorage(string storage) => this.storage = storage;
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine("Computer Configuration:");
+        Console.WriteLine($"CPU : {cpu}");
+        Console.WriteLine($"RAM : {ram}");
+        Console.WriteLine($"Storage : {storage}");
+    }
+}
+    
+//2.Builder
+public interface Builder
+{
+	void BuildCpu();
+	void BuildRam();
+	void BuildStorage();
+    Computer GetResult();
+}
+
+//3.ConcreteBuilder
+public class GamingComputerBuilder : Builder
+{
+    private Computer computer;
+    public GamingComputerBuilder()=> this.computer = new Computer();
+    public void BuildCpu() => computer.SetCpu("Gaming CPU");
+    public void BuildRam() => computer.SetRam("16GB DDR4");
+    public void BuildStorage() => computer.SetStorage("1TB SSD");
+    public Computer GetResult() => computer;
+}
+
+//4.Director
+public class ComputerDirector
+{
+    public void Construct(Builder builder)
+    {
+        builder.BuildCpu();
+        builder.BuildRam();
+        builder.BuildStorage();
+    }
+}
+
+//5.	Client
+public class Client
+{
+    public static void Main()
+    {
+        GamingComputerBuilder gamingComputerBuilder = new GamingComputerBuilder();
+        ComputerDirector director = new ComputerDirector();
+        director.Construct(gamingComputerBuilder);
+        Computer gamingComputer = gamingComputerBuilder.GetResult();
+        gamingComputer.DisplayInfo();
+    }
+}
+
---
+++ FactoryDesignPatternExercise/LeaveRequestHandler/ILeaveManagement.cs
@@ -0,0 +1,125 @@
+﻿namespace FactoryDesignPatternExercise.LeaveRequestHandler;
+
+public interface ILeaveManagement
+{
+    Task ApplyForLeave();
+    Task<bool> CheckLeaveAvliablity();
+    Task<bool> DeductLeaveBalance();
+}
+public enum LeaveType
+{
+    Casual,
+    Sick,
+    Special,
+    Earned,
+    Maternity,
+    Paternity,
+    LWP
+}
+
+public interface IStandardLeaveManagement : ILeaveManagement;
+public interface IEarnLeaveManagement : ILeaveManagement
+{
+    Task EarnLeaveEncashment();
+}
+public interface IMaternityManagement : ILeaveManagement
+{
+    Task ValidateMaternity();
+}
+public interface ILWPLeaveManagement : ILeaveManagement
+{
+    Task AddLwpAmountToSalaryDeduction();
+}
+
+
+
+public abstract class StandardLeaveManagement : ILeaveManagement
+{
+    public virtual async Task ApplyForLeave()
+    {
+        Console.WriteLine("Standard Leave Application Supmitted");
+        await Task.CompletedTask;
+    }
+
+    public virtual Task<bool> CheckLeaveAvliablity()
+    {
+        Console.WriteLine("Standard Leave Baclanced Checked");
+        return Task.FromResult(true);
+    }
+
+    public virtual Task<bool> DeductLeaveBalance()
+    {
+        Console.WriteLine("Standard Leave Baclanced Deducted");
+        return Task.FromResult(true);
+    }
+}
+public class EarnLeaveManagement : StandardLeaveManagement, IEarnLeaveManagement
+{
+    public async override Task ApplyForLeave()
+    {
+        Console.WriteLine("Earn Leave Application Supmitted");
+        await Task.CompletedTask;
+    }
+
+    public async Task EarnLeaveEncashment()
+    {
+        Console.WriteLine("Earn Leave Encashed Successfully.");
+        await Task.CompletedTask;
+    }
+}
+public class MaternityLeaveManagement : StandardLeaveManagement, IMaternityManagement
+{
+    public async override Task ApplyForLeave()
+    {
+        Console.WriteLine("Earn Leave Application Supmitted");
+        await Task.CompletedTask;
+    }
+
+    public async override Task<bool> CheckLeaveAvliablity()
+    {
+        Console.WriteLine("Maternity Leave Available");
+        return await Task.FromResult(true);
+    }
+
+    public async Task ValidateMaternity()
+    {
+        Console.WriteLine("Valied for Maternity Leave");
+        await Task.CompletedTask;
+    }
+}
+public class LWPLeaveManagement : StandardLeaveManagement, ILWPLeaveManagement
+{
+    public async Task AddLwpAmountToSalaryDeduction()
+    {
+        Console.WriteLine("LWP is add to the Salary Deduction.");
+        await Task.CompletedTask;
+    }
+
+    public async override Task ApplyForLeave()
+    {
+        Console.WriteLine("LWP is Applied Successfully.");
+        await Task.CompletedTask;
+    }
+
+    public async override Task<bool> CheckLeaveAvliablity()
+    {
+        Console.WriteLine("LWP Leave Available");
+        return await Task.FromResult(true);
+    }
+
+    public async override Task<bool> DeductLeaveBalance()
+    {
+        Console.WriteLine("LWP Leave Available");
+        return await Task.FromResult(true);
+    }
+}
+
+
+public class LeaveApplicationDto
+{
+    public LeaveType LeaveType { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string? Details { get; set; }
+    public byte[]? Attachments { get; set; }
+}
